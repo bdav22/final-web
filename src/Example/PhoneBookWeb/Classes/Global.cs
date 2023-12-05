8bbb4e05d87583b080dde62fa16b2e5d8528c4d7
bdav22 <californiarock33@gmail.com>
2023-12-05 11:42:37 -0500

    api

--- src/Example/PhoneBookWeb/Classes/Global.cs
+++ src/Example/PhoneBookWeb/Classes/Global.cs
@@ -8,13 +8,13 @@ static class Global
 
     public static string Root { get; private set; }
 
-    public static List<Task> Contacts = new();
+    public static List<Task> Tasks = new();
 
     public static void Add(Task contact)
     {
 	    s_id++;
         contact.Id = s_id;
-        Contacts.Add(contact);
+        Tasks.Add(contact);
         SaveContactsToFile();
     }
 
@@ -22,31 +22,31 @@ static class Global
     {
         Root = root;
 
-        var filePath = Path.Combine(Root, "Contacts.json");
+        var filePath = Path.Combine(Root, "Tasks.json");
         if (File.Exists(filePath))
         {
             var json = File.ReadAllText(filePath);
-            Contacts = JsonConvert.DeserializeObject<List<Task>>(json)!;
-            s_id = Contacts.Max(c => c.Id);
+            Tasks = JsonConvert.DeserializeObject<List<Task>>(json)!;
+            s_id = Tasks.Max(c => c.Id);
         }
     }
 
     public static void SaveContactsToFile()
     {
-        var json = JsonConvert.SerializeObject(Contacts, Formatting.Indented);
-        var filePath = Path.Combine(Root, "Contacts.json");
+        var json = JsonConvert.SerializeObject(Tasks, Formatting.Indented);
+        var filePath = Path.Combine(Root, "Tasks.json");
 
         File.WriteAllText(filePath, json);
     }
 
     public static Task GetContactById(int id)
     {
-	    return Contacts.Single(c => c.Id == id);
+	    return Tasks.Single(c => c.Id == id);
     }
 
     public static void DeleteContactById(int id)
     {
-        Contacts.RemoveAll(c => c.Id == id);
+        Tasks.RemoveAll(c => c.Id == id);
         SaveContactsToFile();
     }
 }
--- src/Example/PhoneBookWeb/Controllers/MainController.cs
+++ src/Example/PhoneBookWeb/Controllers/MainController.cs
@@ -4,27 +4,65 @@ using Microsoft.AspNetCore.Mvc;
 namespace TaskManager.Controllers
 {
     [ApiController]
-    [Route("api/[controller]/[action]")]
+    [Route("api/[controller]")]
     public class MainController : ControllerBase
     {
-        public List<Task> GetContacts() => Global.Contacts;
+        [HttpGet("[action]")]
+        public List<Task> GetTasks() => Global.Tasks;
 
-        public string Add(
-            string description,
-            DateTime dueDate,
-            Boolean isCompleted,
-            DateTime completionDate
-           )
+        [HttpPost("[action]")]
+        public IActionResult Add(
+            [FromBody] Task newTask
+        )
         {
-            Global.Add(new Task
+            Global.Add(newTask);
+            return Ok("Success");
+        }
+
+        [HttpGet("[action]/{id}")]
+        public IActionResult GetTaskById(int id)
+        {
+            var task = Global.GetContactById(id);
+
+            if (task == null)
             {
-                Description = description,
-                DueDate = dueDate,
-                IsCompleted = isCompleted,
-                CompletionDate = completionDate,
-                
-            });
-            return "Success";
+                return NotFound(); // Return 404 if the task with the given ID is not found
+            }
+
+            return Ok(task);
+        }
+
+        [HttpPut("[action]/{id}/description")]
+        public IActionResult ChangeTaskDescription(int id, [FromBody] string newDescription)
+        {
+            var task = Global.GetContactById(id);
+
+            if (task == null)
+            {
+                return NotFound(); // Return 404 if the task with the given ID is not found
+            }
+
+            task.Description = newDescription;
+            Global.SaveContactsToFile();
+
+            return Ok(task);
+        }
+
+        [HttpPut("[action]/{id}/complete")]
+        public IActionResult MarkTaskAsCompleted(int id)
+        {
+            var task = Global.GetContactById(id);
+
+            if (task == null)
+            {
+                return NotFound(); // Return 404 if the task with the given ID is not found
+            }
+
+            task.IsCompleted = true;
+            task.CompletionDate = DateTime.Now;
+            Global.SaveContactsToFile();
+
+            return Ok(task);
         }
     }
 }
